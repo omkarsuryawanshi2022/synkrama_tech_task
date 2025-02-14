deb383bff8c538cd1c4f5a2fb536a663125bc4c4
Omkar Suryawanshi <omkar.suryawanshistc@gmail.com>
2025-02-14 10:29:44 +0530

    apk file add in folder directory share sucess.

--- lib/core/app_colors/app_colors.dart
+++ lib/core/app_colors/app_colors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+
   static const Color primaryColor = Colors.blueAccent;
   static const Color backgroundColor = Color(0xFFE3F2FD);
   static const Color textColor = Colors.black;
